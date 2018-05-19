45e943b52561622c2482fc4d87ce8a11b996ec50
sjohnston88 <iam@samjohnston.co.uk>
2018-05-19 11:26:01 +0100

    remove some files + add basic tests

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./Components/Main";
 import registerServiceWorker from "./registerServiceWorker";
-import "./index.css";
 
 ReactDOM.render(<Main />, document.getElementById("root"));
 registerServiceWorker();
